ab017c2d93b604c7bc1a850a34d47451540fd14a
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-03-05 18:00:49 -0800

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const express = require('express');
+
 // TEMP: Simple In-Memory Database
 const data = require('./db/notes');
 const simDB = require('./db/simDB');
@@ -8,3 +10,28 @@ const notes = simDB.initialize(data);
 console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+  if (req.query.searchTerm) {
+    const searchTerm = req.query.searchTerm;
+    const filteredData = data.filter(item => item.title.includes(searchTerm));
+    res.json(filteredData);
+  } else {
+    res.json(data);
+  }
+});
+  
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = data.find(item => item.id === parseInt(id));
+  res.json(note);
+}); 
